2fdbc814f1961e66e35f26b44646378907f4119e
Yannick KIKI <seyive.kiki@gmail.com>
2020-02-24 18:45:53 +0100

    setup graphql client

--- src/main.js
+++ src/main.js
@@ -3,11 +3,46 @@ import App from "./App.vue";
 import router from "./router";
 import AuthPlugin from "./plugins/auth";
 
+import VueApollo from "vue-apollo";
+
+import ApolloClient from "apollo-client";
+import { HttpLink } from "apollo-link-http";
+import { InMemoryCache } from "apollo-cache-inmemory";
+
 Vue.use(AuthPlugin);
+Vue.use(VueApollo);
 
 Vue.config.productionTip = false;
 
+const getHeaders = () => {
+  const headers = {};
+  const token = window.localStorage.getItem('apollo-token');
+  if (token) {
+    headers.authorization = `Bearer ${token}`;
+  }
+  return headers;
+};
+
+// Create an http link:
+const link = new HttpLink({
+  uri: 'https://hasura.io/learn/graphql',
+  fetch,
+  headers: getHeaders()
+});
+
+const client = new ApolloClient({
+  link: link,
+  cache: new InMemoryCache({
+    addTypename: true
+  })
+});
+
+const apolloProvider = new VueApollo({
+  defaultClient: client,
+});
+
 new Vue({
   router,
+  apolloProvider,
   render: h => h(App)
 }).$mount("#app");
